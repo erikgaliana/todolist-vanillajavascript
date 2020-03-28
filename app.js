c7f5187a4901908b51ca78b0c60c9668ea6fef1c
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2020-03-28 12:29:25 +0100

    save task in local storage

--- app.js
+++ app.js
@@ -7,8 +7,24 @@ function saveTask(e){
     let title=document.getElementById("title").value;
     let description=document.getElementById("description").value;
 
-    console.log( title );
-    console.log( description);
+    
+
+    const task= { title, description};
+    
+    
+
+    // save the task in the tasks object in local storage
+    //localStorage.setItem('tasks', JSON.stringify(task));
+
+    if ( localStorage.getItem('tasks')===null){
+        let tasks=[];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+       let tasks= JSON.parse(localStorage.getItem('tasks'));
+       tasks.push(task);
+       localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 
     e.preventDefault();
 }
